db3de765852471d3cb2c39b209b5dc997ef43b64
davide.modolo <davide.modolo@ec42006c-5374-3e39-d041-fb8c21afcf66>
2011-01-25 11:06:16 +0000

    readIm

--- trunk/SourceCode/Matlab/readImageFromDatabase.m
+++ trunk/SourceCode/Matlab/readImageFromDatabase.m
@@ -5,7 +5,8 @@ function im = readImageFromDatabase(string)
     first_level = matchstr{1,1}(1);
     second_level = strcat(matchstr{1,1}(1), '-', matchstr{1,1}(2));
     third_level = strcat(second_level, '-', matchstr{1,1}(3), '-', matchstr{1,1}(4));
+    
 
-    im = imread(sprintf('%s%s%s%s%s%s%s%s%s.png', '..', pathsep, 'Database', pathsep, first_level{1}, pathsep, second_level{1}, pathsep, third_level{1}));
+    im = imread(sprintf('%s%s%s%s%s%s%s%s%s.png', '..', filesep, 'Database', filesep, first_level{1}, filesep, second_level{1}, filesep, third_level{1}));
 
 end
