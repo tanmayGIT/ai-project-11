d04d98ff7bea90b3ae4a72165f2458e81c29eb7c
davide.modolo <davide.modolo@ec42006c-5374-3e39-d041-fb8c21afcf66>
2011-01-27 08:03:58 +0000

    new_ann

--- SourceCode/Matlab/pipeline.m
+++ SourceCode/Matlab/pipeline.m
@@ -4,9 +4,10 @@
 %   load markov_models.mat;
 %     load models_with_noise.mat;
     load newANNOTATION.mat;
+    load models_1_and_5.mat;
 
   train_set = 30;
-  test_set = 10;
+  test_set = 5;
   
 % %   For every word in the training set
 %   for w_indx = 1:195
@@ -38,9 +39,9 @@
 %   end
 %    
     disp('Evaluating results');
-  for w_indx=10:195
+  for w_indx=1:195
       fprintf('\nCURRENT WORD: %s \nImage: ', words{w_indx});
-      for i = train_set + 3 : (train_set + test_set)
+      for i = train_set + 1 : (train_set + test_set)
         fprintf(' %d ', i);
         % Read Image
         im = readImageFromDatabase(annotations{w_indx, i});
@@ -64,43 +65,48 @@
         ranked_words1{w_indx, i - train_set} = most_likely_words;
         ranked_likelihoods1{w_indx, i - train_set} = likelihoods;
       end
+      
+    save ranked_words1
+    save ranked_likelihoods1
   end
   
-  save ranked_words1
-  save ranked_likelihoods1
-  
-      disp('Evaluating results');
-  for w_indx=1:195
-      fprintf('\nCURRENT WORD: %s \nImage: ', words{w_indx});
-      for i = train_set + 1 : (train_set + test_set)
-        fprintf(' %d ', i);
-        % Read Image
-        im = readImageFromDatabase(annotations{w_indx, i});
-          
-        % Preprocessing
-        word_structure(i) = preProcessing(im);
-          
-        % Feature extraction
-        [statistics, word_features] = featureExtractionInWord(word_structure(i).skeleton); %#ok<ASGLU>
-        % Parameters
-        width = 3;
-        interval = 1;
-            
-        %Evaluate the image againts every model, return a vector of words
-        %ordered by likelihood and another of corresponding likelihoods
-        num_features = 12;
-        test_features = slidingWindow(width, interval, word_structure(i), word_features, num_features)';
-        
-        [most_likely_words, likelihoods] = evaluateObservations(test_features, models_5g, words);
-        
-        ranked_words2{w_indx, i - train_set} = most_likely_words;
-        ranked_likelihoods2{w_indx, i - train_set} = likelihoods;
-      end
-  end
- 
-  save ranked_words2
-  save ranked_likelihood2
-%   evaluateResults(ranked_words, ranked_likelihoods, words);
-  
-  k = 10;
-% end
+%   save ranked_words1
+%   save ranked_likelihoods1
+%   
+%       disp('Evaluating results');
+%   for w_indx=1:195
+%       fprintf('\nCURRENT WORD: %s \nImage: ', words{w_indx});
+%       for i = train_set + 1 : (train_set + test_set)
+%         fprintf(' %d ', i);
+%         % Read Image
+%         im = readImageFromDatabase(annotations{w_indx, i});
+%           
+%         % Preprocessing
+%         word_structure(i) = preProcessing(im);
+%           
+%         % Feature extraction
+%         [statistics, word_features] = featureExtractionInWord(word_structure(i).skeleton); %#ok<ASGLU>
+%         % Parameters
+%         width = 3;
+%         interval = 1;
+%             
+%         %Evaluate the image againts every model, return a vector of words
+%         %ordered by likelihood and another of corresponding likelihoods
+%         num_features = 12;
+%         test_features = slidingWindow(width, interval, word_structure(i), word_features, num_features)';
+%         
+%         [most_likely_words, likelihoods] = evaluateObservations(test_features, models_5g, words);
+%         
+%         ranked_words2{w_indx, i - train_set} = most_likely_words;
+%         ranked_likelihoods2{w_indx, i - train_set} = likelihoods;
+%       end
+% %     save ranked_words2
+% %     save ranked_likelihood2
+%   end
+%  
+%   save ranked_words2
+%   save ranked_likelihood2
+% %   evaluateResults(ranked_words, ranked_likelihoods, words);
+%   
+%   k = 10;
+% % end
